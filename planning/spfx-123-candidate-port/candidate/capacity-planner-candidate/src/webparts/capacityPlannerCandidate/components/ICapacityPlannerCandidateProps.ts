a81e04ddf6ba01da70f48ec70fab964ecee85562
jameslatto-droid <jameslatto@googlemail.com>
2026-06-10 02:31:33 +0200

    Add SPFx 1.23 candidate planning scaffold

---
+++ planning/spfx-123-candidate-port/candidate/capacity-planner-candidate/src/webparts/capacityPlannerCandidate/components/ICapacityPlannerCandidateProps.ts
@@ -0,0 +1,7 @@
+export interface ICapacityPlannerCandidateProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
